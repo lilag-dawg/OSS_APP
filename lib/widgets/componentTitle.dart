6d7d31d6be2588cce4622a61163d15f20e421617
Isam Zerouali <isam.zerouali@usherbrooke.ca>
2020-03-13 21:17:24 -0400

    OSS-171: .dart file created

---
+++ lib/widgets/componentTitle.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ComponentTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SizedBox(
+        width: 300,
+        height: 100,
+      ),
+    );
+  }
+}
