d555cfb0c1c0e2c88a0573a62b193cf3ea01100e
CMoreau <carl.moreau@usherbrooke.ca>
2020-04-02 17:45:58 -0400

    V1.0
    Code needs comments and could be largely improved

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,6 +4,7 @@ const double appWidth = 410;
 const double appHeight = 700;
 
 const int blueButtonColor = 0xFF1565C0;
+const int greyButtonColor = 0xFF252525;
 const int backGroundBlue = 0xFF0D47A1;
 
 
