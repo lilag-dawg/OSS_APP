915d7d9394d9dc4a54af7e935ef5250481b4125e
Isam Zerouali <isam.zerouali@usherbrooke.ca>
2020-04-08 10:28:36 -0400

    OSS-177: Add single child scroll view to all pages

--- lib/screens/batteryLevel_screen.dart
+++ lib/screens/batteryLevel_screen.dart
@@ -13,36 +13,38 @@ class BatteryLevelScreen extends StatelessWidget {
         backgroundColor: Color(Constants.blueButtonColor),
         title: Text("Battery Level")
       ),
-      body: Padding(
-        padding: EdgeInsets.all(25.0),
-        child:Column(
-          children: <Widget>[
-            RoutinePage("Cadence sensor", true, 90),
-            SizedBox(height: 80),
-            RoutinePage("Heart beat sensor", true, 10),
-            SizedBox(height: 80),
-            RoutinePage("Power sensor", false, 50),
-            SizedBox(height: 50),
-            Container(
-              width: 200,
-              height: 50,
-              child: RaisedButton(
-                color: Color(Constants.blueButtonColor),
-                child: Text(
-                  "Device management",
-                  style:TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(25.0),
+          child:Column(
+            children: <Widget>[
+              RoutinePage("Cadence sensor", true, 90),
+              SizedBox(height: 80),
+              RoutinePage("Heart beat sensor", true, 10),
+              SizedBox(height: 80),
+              RoutinePage("Power sensor", false, 50),
+              SizedBox(height: 50),
+              Container(
+                width: 200,
+                height: 50,
+                child: RaisedButton(
+                  color: Color(Constants.blueButtonColor),
+                  child: Text(
+                    "Device management",
+                    style:TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
                   ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  onPressed: (){}
                 ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)
-                ),
-                onPressed: (){}
-              ),
-            )
-          ],
-        )
+              )
+            ],
+          )
+        ),
       )
     );
   }
