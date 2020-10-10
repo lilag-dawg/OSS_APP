1a09e33b175e0eefb297fa01383b0def3844b3be
Alexis G <alexis.garant@usherbrooke.ca>
2020-10-10 15:58:19 -0400

    OS-39: working version pour demo audit

--- lib/models/bluetoothDeviceManager.dart
+++ lib/models/bluetoothDeviceManager.dart
@@ -1,9 +1,28 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import './OSSDevice.dart';
 
 class BluetoothDeviceManager extends ChangeNotifier {
   OSSDevice ossDevice;
   BluetoothDeviceManager();
+
+  static String connectionHandlingService = "1802";
+  
+  static String nombreCapteursCharact = "16A1";
+  static String listCapteursCharact = "16A2";
+
+  static String convertRawToStringListCapteursCharact(List<int> value){
+    String name = "";
+    for(int i = 1; i < value.length; i++){
+      if(value[i] == 0)
+        break;
+
+      name = name + String.fromCharCode(value[i]);
+    }
+    return name;
+  }
+
 
 
   void setDevice(OSSDevice device){
@@ -14,6 +33,5 @@ class BluetoothDeviceManager extends ChangeNotifier {
   void remove(){
     ossDevice = null;
   }
-
 
 }
