2ef9915c2c078506e82af3f1554fdf7166473dd5
Alexis G <alexis.garant@usherbrooke.ca>
2020-11-03 11:21:34 -0500

    minor changes

--- lib/screens/manageDevicesScreen.dart
+++ lib/screens/manageDevicesScreen.dart
@@ -11,19 +11,22 @@ class ManageDevicesScreen extends StatefulWidget {
 }
 
 class _ManageDevicesScreenState extends State<ManageDevicesScreen> {
-  List<String> connectedDevices = [];
+  List<String> pairedDevices = [];
   List<String> availableDevices = [];
-
 
   String paired = "Paired";
   String notPaired = "notPaired";
 
-  List<Widget> _buildConnectedDevicesTiles(List<String> result, BluetoothDeviceManager ossManager) {
-    return result.map((d) => _customCardTile(d, paired, ossManager)).toList();
+  List<Widget> _buildPairedDevicesTiles(
+      List<String> result, BluetoothDeviceManager ossManager) {
+    return result.map((d) => _customListTile(d, paired, ossManager)).toList();
   }
 
-  List<Widget> _buildAvailableDevicesTiles(List<String> result, BluetoothDeviceManager ossManager) {
-    return result.map((d) => _customCardTile(d, notPaired, ossManager)).toList();
+  List<Widget> _buildAvailableDevicesTiles(
+      List<String> result, BluetoothDeviceManager ossManager) {
+    return result
+        .map((d) => _customListTile(d, notPaired, ossManager))
+        .toList();
   }
 
   Future<void> _getList(Stream<List<int>> stream, int numberOfsensor) async {
@@ -34,14 +37,13 @@ class _ManageDevicesScreenState extends State<ManageDevicesScreen> {
         if (count == numberOfsensor) break;
         currentName =
             BluetoothDeviceManager.convertRawToStringListCapteursCharact(value);
-        if(value[0] & 0x01 == 1){
-          if (!connectedDevices.contains(currentName)){
-            connectedDevices.add(currentName);
+        if (value[0] & 0x01 == 1) {
+          if (!pairedDevices.contains(currentName)) {
+            pairedDevices.add(currentName);
             count = count + 1;
           }
-        }
-        else{
-          if (!availableDevices.contains(currentName)){
+        } else {
+          if (!availableDevices.contains(currentName)) {
             availableDevices.add(currentName);
             count = count + 1;
           }
@@ -50,22 +52,22 @@ class _ManageDevicesScreenState extends State<ManageDevicesScreen> {
     }
   }
 
-  Future<int> _getNumberOfSensorCharact(BluetoothDeviceManager ossManager) async{
-
-      BluetoothDeviceCharacteristic nombreCapteursCharact = ossManager.ossDevice
+  Future<int> _getNumberOfSensorCharact(
+      BluetoothDeviceManager ossManager) async {
+    BluetoothDeviceCharacteristic nombreCapteursCharact = ossManager.ossDevice
         .getService(BluetoothDeviceManager.connectionHandlingService)
         .getCharacteristic(BluetoothDeviceManager.nombreCapteursCharact);
 
-      int number = 0;
+    int number = 0;
 
-      await nombreCapteursCharact.characteristic.read().then((value) => number = BluetoothDeviceManager.convertRawToIntCapteursCharact(value));
+    await nombreCapteursCharact.characteristic.read().then((value) =>
+        number = BluetoothDeviceManager.convertRawToIntCapteursCharact(value));
 
-      return number;
+    return number;
   }
 
   Future<void> _getSensorStringListFromCharact(
       BluetoothDeviceManager ossManager) async {
-
     BluetoothDeviceCharacteristic listCapteursCharact = ossManager.ossDevice
         .getService(BluetoothDeviceManager.connectionHandlingService)
         .getCharacteristic(BluetoothDeviceManager.listCapteursCharact);
@@ -73,7 +75,8 @@ class _ManageDevicesScreenState extends State<ManageDevicesScreen> {
     int numberOfsensor = 0;
 
     if (listCapteursCharact != null) {
-      await _getNumberOfSensorCharact(ossManager).then((value) => numberOfsensor = value);
+      await _getNumberOfSensorCharact(ossManager)
+          .then((value) => numberOfsensor = value);
       await listCapteursCharact.characteristic.setNotifyValue(true);
       await _getList(listCapteursCharact.characteristic.value, numberOfsensor)
           .then((value) async {
@@ -82,104 +85,112 @@ class _ManageDevicesScreenState extends State<ManageDevicesScreen> {
     }
   }
 
-  Future<void> _writeToMCU(String deviceName, String status, BluetoothDeviceManager ossManager) async{
-      BluetoothDeviceCharacteristic paringRequestCharact = ossManager.ossDevice
+  Future<void> _writeToMCU(String deviceName, String status,
+      BluetoothDeviceManager ossManager) async {
+    BluetoothDeviceCharacteristic paringRequestCharact = ossManager.ossDevice
         .getService(BluetoothDeviceManager.connectionHandlingService)
         .getCharacteristic(BluetoothDeviceManager.paringRequestCharact);
 
-      await paringRequestCharact.characteristic.write(BluetoothDeviceManager.sendPairingRequestCharact(deviceName, status)).then((value) => print(value));
+    await paringRequestCharact.characteristic
+        .write(BluetoothDeviceManager.sendPairingRequestCharact(
+            deviceName, status))
+        .then((value) => print(value));
   }
 
-  Future<void> _handlePressTrailing(String deviceName, String status, BluetoothDeviceManager ossManager) async{
-    await _writeToMCU(deviceName, status, ossManager).then((value){
+  Future<void> _handlePressTrailing(String deviceName, String status,
+      BluetoothDeviceManager ossManager) async {
+    await _writeToMCU(deviceName, status, ossManager).then((value) {
       setState(() {
-        connectedDevices.clear();
+        pairedDevices.clear();
         availableDevices.clear();
       });
     });
   }
 
-  Widget _customCardTile(String deviceName, String status, BluetoothDeviceManager ossManager) {
-    return Card(
-        color: Colors.black12,
-        child: ListTile(
-          leading: (status == paired)
-              ? Icon(Icons.bluetooth_connected)
-              : Icon(Icons.bluetooth_disabled),
-          title: Text(deviceName),
-          subtitle: (status == paired) ? Text('Active') : SizedBox.shrink(),
-          trailing: RaisedButton(
-              child: (status == paired) ? Text("Forget") : Text("Pair"),
-              onPressed: () async {
-                await _handlePressTrailing(deviceName, status, ossManager);
-              }),
-        ));
+  Widget _customListTile(
+      String deviceName, String status, BluetoothDeviceManager ossManager) {
+    return ListTile(
+      leading: (status == paired)
+          ? Icon(Icons.bluetooth_connected)
+          : Icon(Icons.bluetooth_disabled),
+      title: Text(deviceName),
+      subtitle: (status == paired) ? Text('Active') : SizedBox.shrink(),
+      trailing: _customTrailing(deviceName, status, ossManager),
+    );
+  }
+
+  Widget _customTrailing(
+      String deviceName, String status, BluetoothDeviceManager ossManager) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(left: BorderSide(width:1, color: Colors.black45))
+      ),
+      padding: EdgeInsets.only(left:5),
+      child: RaisedButton(
+          child: (status == paired) ? Text("Forget") : Text("Pair"),
+          onPressed: () async {
+            await _handlePressTrailing(deviceName, status, ossManager);
+          }),
+    );
+  }
+
+  Widget _customBox(BluetoothDeviceManager ossManager, String title,
+      List<Widget> widgetList) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          color: Colors.black45,
+          width: 1,
+        ),
+      ),
+      child: Column(children: [
+        Align(
+          alignment: FractionalOffset(0.1, 0),
+          child: Container(
+            margin: EdgeInsets.only(top: 10, bottom: 5),
+            child: Text(
+              title,
+              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey),
+            ),
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          height: 1.5,
+          color: Colors.black54,
+        ),
+        Column(
+          children: widgetList,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+        )
+      ]),
+    );
   }
 
   Widget _buildMainBody(BluetoothDeviceManager ossManager) {
     return Container(
       child: Column(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(
-                color: Colors.black45,
-                width: 1,
-              ),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Column(children: [
-              Align(
-                alignment: FractionalOffset(0.1, 0),
-                child: Text(
-                  "Connected Devices",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.grey),
-                ),
-              ),
-              Column(children: _buildConnectedDevicesTiles(connectedDevices, ossManager))
-            ]),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(
-                color: Colors.black45,
-                width: 1,
-              ),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Column(children: [
-              Align(
-                alignment: FractionalOffset(0.1, 0),
-                child: Text(
-                  "Available Devices",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.grey),
-                ),
-              ),
-              Column(children: _buildAvailableDevicesTiles(availableDevices, ossManager))
-            ]),
-          ),
+          _customBox(ossManager, "Available Devices",
+              _buildAvailableDevicesTiles(availableDevices, ossManager)),
+          _customBox(ossManager, "Paired Devices",
+              _buildPairedDevicesTiles(pairedDevices, ossManager)),
         ],
       ),
     );
   }
-
 
   Widget _buildBody(BluetoothDeviceManager ossManager) {
     return FutureBuilder<void>(
       future: _getSensorStringListFromCharact(ossManager),
       builder: (c, snapshot) {
         print(snapshot.connectionState);
-        if(snapshot.connectionState == ConnectionState.done){
-            return _buildMainBody(ossManager);
-        }
-        else{
+        if (snapshot.connectionState == ConnectionState.done) {
+          return _buildMainBody(ossManager);
+        } else {
           return Center(child: CircularProgressIndicator());
         }
-
       },
     );
   }
